ca12d5216137e8f07d4c7154d96f6fd8eba4d139
kevvo23 <kssalamea@gmail.com>
2025-03-28 16:08:45 +0100

    new button added in order to delete

--- Website/JavaScript/JavaScriptPrincipal.js
+++ Website/JavaScript/JavaScriptPrincipal.js
@@ -1,7 +1,11 @@
 window.onload = function () {
   const buttonAddData = document.getElementById("buttonAddUser");
+  const buttonDeleteData = document.getElementById("buttonDeleteUser");
 
   buttonAddData.onclick = function () {
     window.open("../HTML/PopUp.html", "_blank", "width=631,height=300");
+  };
+  buttonDeleteData.onclick = function () {
+    window.open("../HTML/SelectUser.html", "_blank", "width=631,height=300");
   };
 };
