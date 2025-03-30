96a1c0719a6c0c2130f37526c02f87db732467dd
Ekaitz Campo <ekaitzcampo2002@gmail.com>
2025-03-30 15:00:29 +0200

    Fixing reload error

--- Website/JavaScript/JavaScriptPrincipal.js
+++ Website/JavaScript/JavaScriptPrincipal.js
@@ -1,9 +1,15 @@
 window.onload = function () {
-  // Realiza la solicitud fetch al archivo PHP
+  if (localStorage.getItem("hasLoaded")) {
+    localStorage.removeItem("hasLoaded");
+    return;
+  }
+
   fetch("http://localhost/M.A.E.K.-LMS-/basex/BDExport.php")
     .then((response) => response.json())
     .then((data) => {
       console.log("Datos recibidos:", data);
+      localStorage.setItem("hasLoaded", "true");
+      window.location.reload();
     })
     .catch((error) => {
       console.error("Error al hacer fetch:", error);
